cfc3b9e068724ccd5d7c8396c328c545b1979fc5
yugankchoksi15 <yugankchoksi15@gmail.com>
2024-12-04 11:04:54 +0530

    swagger integration

--- server/claim-management/src/main.ts
+++ server/claim-management/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Claim settlement Demo APIs')
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
