2b6de1374b32835a5b2fb2246c4331eed24d4cdf
yugankchoksi15 <yugankchoksi15@gmail.com>
2024-12-05 17:45:22 +0530

    fixed dialog box design

--- frontend/src/app/components/RepairCenterDialog/RepairCenterDialog.tsx
+++ frontend/src/app/components/RepairCenterDialog/RepairCenterDialog.tsx
@@ -71,7 +71,7 @@ const RepairCenterDialog: React.FC<RepairCenterDialogProps> = ({
     <div
       className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ${isOpen ? "block" : "hidden"}`}
     >
-      <div className="bg-white rounded-lg shadow-lg w-96 p-6 relative">
+      <div className="bg-white rounded-lg shadow-lg w-[35rem] p-10 relative">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
